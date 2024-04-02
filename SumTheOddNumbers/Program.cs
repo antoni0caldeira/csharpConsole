f6d9b7c1c6b2c62d025583c65ddb0cdbbb30ec63
Antonio Caldeira <antoniocaldeira@yahoo.com>
2024-04-02 15:17:39 +0100

    Sum of the odd numbers 1st iteration

---
+++ SumTheOddNumbers/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Sum the odd numbers");
+
+// Implement an algorithm to sum together all the odd numbers in a collection. 
+
+int[] lista = {1, 2, 3, 4, 5};
+int sum = 0;
+
+foreach(var n in lista)
+{
+sum += n % 2 != 0 ? n : 0;
+}
+System.Console.WriteLine(sum);
