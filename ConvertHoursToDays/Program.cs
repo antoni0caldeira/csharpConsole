d37e7994a1000500c6f3e9e3fbd578b7fdd91577
Antonio Caldeira <antoniocaldeira@yahoo.com>
2024-04-03 18:03:11 +0100

    Convert hour to days 1st iteration

---
+++ ConvertHoursToDays/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Convert hours to days");
+
+
+// convert hours into days. Here is the basic formula: days = h / 24.
+
+System.Console.WriteLine("Please insert the number of hours to convert");
+int hours = int.Parse(Console.ReadLine());
+
+int days = hours / 24; // get the number of whole days
+int remainingHours = hours % 24; // get the remaining hours
+
+Console.WriteLine($"{days} day(s) and {remainingHours} hour(s)");
